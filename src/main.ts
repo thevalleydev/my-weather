a64fd84ce3dd91a022f8f7e69e8d36dea7c463cf
thevalleydev <77467961+thevalleydev@users.noreply.github.com>
2024-02-03 13:32:43 -0500

    remove onload and add top level !document

--- src/main.ts
+++ src/main.ts
@@ -1,97 +1,93 @@
 export default (() => {
+  if (!document) return
   // Create a class for the element
-class MyWeather extends HTMLElement {
-  weatherApiUrl: string
-  doRender: boolean
-  forecastData: Array<any>
-  forecastMarkup: string
-  contentDiv: any
-  getForecast: any
-  getForecastUrl: any
-  
-  constructor() {
-    // Always call super first in constructor
-    super()
-    this.doRender = true
-    this.weatherApiUrl = `https://api.weather.gov/points/${this.getAttribute('lat')},${this.getAttribute('lng')}`
-    this.contentDiv = <HTMLElement>document.createElement('div')
-    this.contentDiv.classList.add('periodContainer')
-    this.contentDiv.setAttribute('style', 'display:flex;')
-    this.contentDiv.innerText = 'loading'
-    this.forecastData = []
-    this.forecastMarkup = ''
+  class MyWeather extends HTMLElement {
+    weatherApiUrl: string
+    doRender: boolean
+    forecastData: Array<any>
+    forecastMarkup: string
+    contentDiv: any
+    getForecast: any
+    getForecastUrl: any
     
-    this.getForecast = async (url: RequestInfo | URL) => {
-        const response = await fetch(url)
+    constructor() {
+      // Always call super first in constructor
+      super()
+      this.doRender = true
+      this.weatherApiUrl = `https://api.weather.gov/points/${this.getAttribute('lat')},${this.getAttribute('lng')}`
+      this.contentDiv = <HTMLElement>document.createElement('div')
+      this.contentDiv.classList.add('periodContainer')
+      this.contentDiv.setAttribute('style', 'display:flex;')
+      this.contentDiv.innerText = 'loading'
+      this.forecastData = []
+      this.forecastMarkup = ''
+      
+      this.getForecast = async (url: RequestInfo | URL) => {
+          const response = await fetch(url)
+          const weather = await response.json()
+          return weather?.properties
+      }
+      this.getForecastUrl = async () => {
+        const response = await fetch(this.weatherApiUrl)
         const weather = await response.json()
-        return weather?.properties
+        return this.getForecast(weather?.properties?.forecast)
+      }
     }
-    this.getForecastUrl = async () => {
-      const response = await fetch(this.weatherApiUrl)
-      const weather = await response.json()
-      return this.getForecast(weather?.properties?.forecast)
+    
+    connectedCallback() {
+      const shadow = this.attachShadow({ mode: "open" })
+      shadow.innerHTML = `<style>
+        :host {
+          font: 1.2rem sans-serif;
+          max-width: 400px;
+          display: block;
+        }
+        h1 {
+          font-weight: 500;
+        }
+        .periodContainer {
+          justify-content: space-between;
+        }
+        .periodCard {
+          display: flex;
+          flex-direction: column;
+        }
+        .temperature {
+            text-align: center;
+            font-size: 1.8rem;
+            padding: .5rem;
+        }
+        </style>
+        <h1><slot name="title">My Weather</slot></h1>
+      `
+      shadow.appendChild(this.contentDiv)
+      
+      this.getForecastUrl().then((forecast: { periods: any; }) => {
+        this.mapData(forecast).buildUi().render()
+      })
+    }
+    
+    render() {
+      this.doRender = false
+      this.contentDiv.innerHTML = this.forecastMarkup
+    }
+    
+    mapData(forecast: { periods: any; }) {
+      const { periods } = forecast
+      
+      this.forecastData = periods.slice(0, 3).map((period: { name: any; temperature: any; temperatureUnit: any; windSpeed: any; shortForecast: any; }) => {
+        const { name, temperature, temperatureUnit, windSpeed, shortForecast } = period
+        return { name, temperature, temperatureUnit, windSpeed, shortForecast}
+      })
+      return this
+    }
+    
+    buildUi() {
+      this.forecastMarkup = this.forecastData.map((periodData) => {
+        return `<div class="periodCard"><div>${periodData.name}</div><div class="temperature">${periodData.temperature}</div></div>`
+      }).join('')
+      return this
     }
   }
-  
-  connectedCallback() {
-    const shadow = this.attachShadow({ mode: "open" })
-    shadow.innerHTML = `<style>
-      :host {
-        font: 1.2rem sans-serif;
-        max-width: 400px;
-        display: block;
-       }
-       h1 {
-         font-weight: 500;
-       }
-       .periodContainer {
-         justify-content: space-between;
-       }
-       .periodCard {
-         display: flex;
-         flex-direction: column;
-       }
-       .temperature {
-          text-align: center;
-          font-size: 1.8rem;
-          padding: .5rem;
-       }
-      </style>
-      <h1><slot name="title">My Weather</slot></h1>
-    `
-    shadow.appendChild(this.contentDiv)
-    
-    this.getForecastUrl().then((forecast: { periods: any; }) => {
-      this.mapData(forecast).buildUi().render()
-    })
-  }
-  
-  render() {
-    this.doRender = false
-    this.contentDiv.innerHTML = this.forecastMarkup
-  }
-  
-  mapData(forecast: { periods: any; }) {
-    const { periods } = forecast
-    
-    this.forecastData = periods.slice(0, 3).map((period: { name: any; temperature: any; temperatureUnit: any; windSpeed: any; shortForecast: any; }) => {
-      const { name, temperature, temperatureUnit, windSpeed, shortForecast } = period
-      return { name, temperature, temperatureUnit, windSpeed, shortForecast}
-    })
-    return this
-  }
-  
-  buildUi() {
-    this.forecastMarkup = this.forecastData.map((periodData) => {
-      return `<div class="periodCard"><div>${periodData.name}</div><div class="temperature">${periodData.temperature}</div></div>`
-    }).join('')
-    return this
-  }
-}
-if (document) {
-  window.onload = (() => {
-
-    customElements.define("my-weather", MyWeather);
-  })
-}
+  customElements.define("my-weather", MyWeather);
 })()
